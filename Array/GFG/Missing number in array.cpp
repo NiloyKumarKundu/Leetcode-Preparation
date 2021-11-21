651162b1f2d3992e34a552523cf629785d09560d
Niloy <nkundu191071@bscse.uiu.ac.bd>
2021-11-21 12:57:18 +0600

    Full-Preparation

--- Array/GFG/Missing number in array.cpp
+++ Array/GFG/Missing number in array.cpp
@@ -1,3 +1,6 @@
+// Time: O(N) Space: O(1) Issue: No overlapping
+
+
 class Solution{
   public:
     int MissingNumber(vector<int>& arr, int n) {
@@ -11,5 +14,22 @@ class Solution{
         
         ans ^= (i + 1);
         return (temp ^ ans);
+    }
+};
+
+// Time: O(N) Space: O(1) Issue: Overlapping
+
+
+class Solution{
+  public:
+    int MissingNumber(vector<int>& arr, int n) {
+        // Your code goes here
+        int ans = 0, temp = 0, i;
+		ans = (n * (n + 1)) / 2;
+
+		for (i = 0; i < n - 1; i++) {
+            temp += arr[i];
+        }
+        return ans - temp;
     }
 };
