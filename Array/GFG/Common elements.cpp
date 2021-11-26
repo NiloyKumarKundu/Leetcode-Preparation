65773b1de62518dc365676f545c6e4c23c0445d6
Niloy <nkundu191071@bscse.uiu.ac.bd>
2021-11-26 11:43:50 +0600

    Full-Preparation

--- Array/GFG/Common elements.cpp
+++ Array/GFG/Common elements.cpp
@@ -1,4 +1,45 @@
 // Time Complixity: O(n1 + n2 + n3)
+// Space Complixity: O(1)
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            //code here.
+            int i = 0, j = 0, k = 0;
+            vector<int> ans;
+            
+            while (i < n1 && j < n2 && k < n3) {
+                if (A[i] == B[j] && B[j] == C[k]) {
+                    ans.push_back(A[i]);
+                    i++;
+                    j++;
+                    k++;
+                } else if (A[i] < B[j]) {
+                    i++;
+                } else if (B[j] < C[k]) {
+                    j++;
+                } else {
+                    k++;
+                }
+                int xx = A[i - 1];
+                while (A[i] == xx) i++;
+                xx = B[j - 1];
+                while (B[j] == xx) j++;
+                xx = C[k - 1];
+                while (C[k] == xx) k++;
+            }
+            
+            if (ans.size() == 0) ans.push_back(-1);
+            return ans;
+            
+        }
+};
+
+
+
+
+// Time Complixity: O(n1 + n2 + n3)
 // Space Complixity: O(n1 + n2 + n3)
 
 class Solution
