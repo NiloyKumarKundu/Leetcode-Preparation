5e543a209014603b6e96d7faa74138e66fc4bee4
Niloy <nkundu191071@bscse.uiu.ac.bd>
2021-11-26 12:42:02 +0600

    Full-Preparation

---
+++ Array/GFG/Non-Repeating Element.cpp
@@ -0,0 +1,22 @@
+// Time Complixity: O(N) On avg
+// Space Complixity: O(N)
+
+class Solution{
+    public:
+    int firstNonRepeating(int arr[], int n) 
+    { 
+        // Complete the function
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[arr[i]]++;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (mp[arr[i]] == 1) {
+                return arr[i];
+            }
+        }
+        return 0;
+    } 
+  
+};
