6337af1b9967c070805d72b2cca60d71eec3bacf
Niloy <nkundu191071@bscse.uiu.ac.bd>
2021-12-03 20:59:01 +0600

    Full-Preparation

---
+++ Array/GFG/Factorials of large numbers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    vector<int> factorial(int N){
+        // code here
+        vector<int> ans;
+        ans.push_back(1);
+        
+        for (int i = 2; i <= N; i++) {
+            int carry = 0;
+            for (int j = 0; j < ans.size(); j++) {
+                int value = ans[j] * i + carry;
+                ans[j] = value % 10;
+                carry = value / 10;
+            }
+            while (carry != 0) {
+                ans.push_back(carry % 10);
+                carry /= 10;
+            }
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
