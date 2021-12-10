0f21e5657c75d5c535b7004533554c05aa240ae9
Niloy <nkundu191071@bscse.uiu.ac.bd>
2021-12-10 21:48:44 +0600

    Full-Preparation

---
+++ Array/GFG/Minimum number of jumps.cpp
@@ -0,0 +1,35 @@
+// Time Complixity: O(N)
+// Space Complixity: O(1)
+
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        // Your code here
+        if (n <= 1) {
+            return 0;
+        }
+        
+        if (arr[0] == 0) {
+            return -1;
+        }
+        
+        int maxReach = arr[0], steps = arr[0], jump = 1;
+        
+        for (int i = 1; i < n; i++) {
+            if (i == n - 1) {
+                return jump;
+            }
+            
+            maxReach = max(maxReach, i + arr[i]);
+            steps--;
+            if (steps == 0) {
+                jump++;
+                if (i >= maxReach) {
+                    return -1;
+                }
+                steps = maxReach - i;
+            }
+        }
+        return -1;
+    }
+};
