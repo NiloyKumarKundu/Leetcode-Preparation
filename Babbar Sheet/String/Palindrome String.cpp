9e50bc8c2ebee2433c61de9fe599d46bd269fab6
Niloy <nkundu191071@bscse.uiu.ac.bd>
2021-12-24 11:57:41 +0600

    Full-Preparation

---
+++ Babbar Sheet/String/Palindrome String.cpp
@@ -0,0 +1,49 @@
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    string temp = S;
+	    reverse(temp.begin(), temp.end());
+	    
+	    if (S == temp) {
+	        return 1;
+	    } else {
+	        return 0;
+	    }
+	    return 0;
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}  // } Driver Code Ends
