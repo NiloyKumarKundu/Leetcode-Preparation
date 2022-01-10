24fb63a196b17b7d38b508a887ff8f42413b8fcf
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-01-10 19:37:30 +0600

    Full-Preparation

--- Daily Leetcode Challenge/January 2022/Day - 4: Complement of Base 10 Integer.cpp
+++ Daily Leetcode Challenge/January 2022/Day - 4: Complement of Base 10 Integer.cpp
@@ -16,3 +16,28 @@ public:
         return result;       
     }
 };
+
+
+
+// Another Solution
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        string str = "";
+        int result = 1;
+        int temp = n;
+        
+        while (temp > 0) {
+            result = temp % 2;
+            temp /= 2;
+            str += to_string(result);
+        }
+
+        int res = (((1 << str.length()) - 1) ^ n);
+        
+        return res;
+    }
+};
