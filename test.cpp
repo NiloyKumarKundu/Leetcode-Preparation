bc0f68d99d7c0eda189d7a2f714000abe35d5a8f
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-01-13 13:46:57 +0600

    Full-Preparation

--- test.cpp
+++ test.cpp
@@ -3,49 +3,9 @@ using namespace std;
 
 
 int main() {
-	int n;
-	cin >> n;
-	int k = n / 2;
-	int a = k, b = k;
-
-	for (int i = 0; i < k; i++) {
-		for (int j = 0; j < n; j++) {
-            if (j == k) {
-				cout << '$';
-			} else if (j == a) {
-				cout << '$';
-			}
-            else if (j == b) {
-				cout << '$';
-			} else {
-				cout << '_';
-			}
-		}
-		cout << endl;
-		a--;
-		b++;
-	}
-	for (int i = 0; i < n; i++) {
-		cout << '$';
-	}
-	cout << endl;
-	a++;
-	b--;
-	for (int i = k + 1; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-            if (j == k) {
-				cout << '$';
-			} else if (j == a) {
-				cout << '$';
-			}
-            else if (j == b) {
-				cout << '$';
-			} else {
-				cout << '_';
-			}
-		}
-		cout << endl;
-		a++;
-		b--;
-	}
+	int x = 1 << 1;
+	x += 0;
+	x += 1 << 1;
+	x += 0;
+	cout << x << endl;
 }
