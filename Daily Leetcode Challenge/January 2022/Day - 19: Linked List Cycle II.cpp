524232ae8b102ddbd3a3cfbb07ad7318a619224d
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-01-19 14:23:19 +0600

    Full-Preparation

---
+++ Daily Leetcode Challenge/January 2022/Day - 19: Linked List Cycle II.cpp
@@ -0,0 +1,43 @@
+// Space Complexity: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *intersection(ListNode *head) {
+        ListNode *hare = head, *tortoris = head;
+        
+        while (hare != NULL && hare->next != NULL) {
+            tortoris = tortoris->next;
+            hare = hare->next->next;
+            if (hare == tortoris) {
+                return hare;
+            }
+        }
+        return NULL;
+    }
+    
+    ListNode *detectCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL) {
+            return NULL;
+        }
+        ListNode *intersect = intersection(head);
+        
+        if (intersect != NULL) {
+            ListNode *start = head;
+            while (intersect != start) {
+                intersect = intersect->next;
+                start = start->next;
+            }
+            return start;
+        }
+        
+        return NULL;
+    }
+};
