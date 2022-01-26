c7f3dff05f2734fc99e55230f5a67bafbc555f4b
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-01-26 19:30:42 +0600

    Full-Preparation

--- Daily Leetcode Challenge/January 2022/Day - 21: Gas Station.cpp
+++ Daily Leetcode Challenge/January 2022/Day - 21: Gas Station.cpp
@@ -1,3 +1,58 @@
+// Optimised Solution && clean code
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int availableGas = 0, has_gas = 0, cur_station = 0;
+        
+        for (int i = 0; i < gas.size(); i++) {
+            int cur_gas = gas[i] - cost[i];
+            availableGas += cur_gas;
+            has_gas += cur_gas;
+            
+            if (has_gas < 0) {
+                has_gas = 0;
+                cur_station = i + 1;
+            }
+        }
+        return availableGas >= 0 ? cur_station : -1;
+    }
+};
+
+
+// Optimised Solution
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int diff = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            diff += (gas[i] - cost[i]);
+        }
+        
+        if (diff < 0) {
+            return -1;
+        }
+        
+        int starting = 0, tank = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            tank = tank + gas[i] - cost[i];
+            if (tank < 0) {
+                starting = i + 1;
+                tank = 0;
+            }
+        }
+        return starting;
+    }
+};
+
+
+
 // Time Complexity: O(N^2)
 // Space Complexity: O(1)
 
@@ -25,30 +80,5 @@ public:
 
 
 
-// Optimised Solution
-// Time Complexity: O(N)
-// Space Complexity: O(1)
 
-class Solution {
-public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int diff = 0;
-        for (int i = 0; i < gas.size(); i++) {
-            diff += (gas[i] - cost[i]);
-        }
-        
-        if (diff < 0) {
-            return -1;
-        }
-        
-        int starting = 0, tank = 0;
-        for (int i = 0; i < gas.size(); i++) {
-            tank = tank + gas[i] - cost[i];
-            if (tank < 0) {
-                starting = i + 1;
-                tank = 0;
-            }
-        }
-        return starting;
-    }
-};
+
