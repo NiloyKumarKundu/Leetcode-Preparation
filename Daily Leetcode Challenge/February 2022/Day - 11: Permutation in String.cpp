53af92460085f77d3d752494d18f80cdca1b441a
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-02-11 16:36:32 +0600

    Full-Preparation

---
+++ Daily Leetcode Challenge/February 2022/Day - 11: Permutation in String.cpp
@@ -0,0 +1,36 @@
+// Time Complexity: O(alphabet_size * length(s2))
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int> s1hash(26, 0);
+        vector<int> s2hash(26, 0);
+        
+        int s1len = s1.length();
+        int s2len = s2.length();
+        
+        if (s1len > s2len)
+            return false;
+        
+        int left = 0, right = 0;
+        
+        while (right < s1len) {
+            s1hash[s1[right] - 'a']++;
+            s2hash[s2[right] - 'a']++;
+            right++;
+        }
+        right--;
+        
+        while (right < s2len) {
+            if (s1hash == s2hash)
+                return true;
+            
+            right++;
+            if (right != s2len)
+                s2hash[s2[right] - 'a']++;
+            s2hash[s2[left] - 'a']--;
+            left++;
+        }
+        return false;
+    }
+};
