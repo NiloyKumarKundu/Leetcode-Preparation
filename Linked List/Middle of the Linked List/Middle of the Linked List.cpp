672de9894fb3d416270bafb4c861a24fe8e3f54b
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-02-24 23:42:30 +0600

    Middle of the Linked List

---
+++ Linked List/Middle of the Linked List/Middle of the Linked List.cpp
@@ -0,0 +1,30 @@
+// Time Complexity: O(N / 2)
+// Space Complexity: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if (head == NULL || head->next == NULL) 
+            return head;
+        
+        ListNode* first = head;
+        ListNode* second = head;
+
+        while (second != NULL && second->next != NULL) {
+            first = first->next;
+            second = second->next->next;
+        }
+        
+        return first;
+    }
+};
