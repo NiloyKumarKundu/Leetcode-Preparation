772cebf568638bc35084abf15c854f98a589c36a
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-02-24 15:03:04 +0600

    MargeSort

--- Linked List/Sorting/MargeSort.cpp
+++ Linked List/Sorting/MargeSort.cpp
@@ -6,28 +6,28 @@ struct node {
 	struct node *next;
 };
 
-typedef struct node Node;
+typedef struct node ListNode;
 
-void insert(Node* &head, int val) {
+void insert(ListNode* &head, int val) {
 	if (head == NULL) {
-		head = new Node();
+		head = new ListNode();
 		head->val = val;
 		head->next = NULL;
 	} else {
-		Node *temp = new Node();
+		ListNode *temp = new ListNode();
 		temp = head;
 		while (temp->next != NULL) {
 			temp = temp->next;
 		}
-		Node *flag = new Node();
+		ListNode *flag = new ListNode();
 		flag->val = val;
 		flag->next = NULL;
 		temp->next = flag;
 	}
 }
 
-void display(Node* &head) {
-	Node *temp = head;
+void display(ListNode* &head) {
+	ListNode *temp = head;
     while (temp != NULL) {
 		cout << temp->val << endl;
 		temp = temp->next;
@@ -35,9 +35,48 @@ void display(Node* &head) {
 }
 
 
+ListNode* middleNode(ListNode* head) {
+	ListNode *slow = head, *fast = head->next;
+	while (fast != NULL && fast->next != NULL) {
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	ListNode *mid = slow->next;
+	slow->next = NULL;
+	return mid;
+}
+
+ListNode* merge(ListNode* l1, ListNode* l2) {
+	ListNode *dummyNode = new ListNode();
+	ListNode *current = dummyNode;
+	while (l1 != NULL && l2 != NULL) {
+		if (l1->val <= l2->val) {
+			current->next = l1;
+			l1 = l1->next;
+		} else {
+			current->next = l2;
+			l2 = l2->next;
+		}
+		current = current->next;
+	}
+	current->next = l1 != NULL ? l1 : l2;
+	return dummyNode->next;
+}
+
+ListNode* mergeSort(ListNode* head) {
+	if (head == NULL || head->next == NULL)
+		return head;
+
+	ListNode *mid = middleNode(head);
+	ListNode *left = mergeSort(head);
+	ListNode *right = mergeSort(mid);
+
+	return merge(left, right);
+}
 
 int main() {
-	Node *head = NULL;
+	ListNode *head = NULL;
 	insert(head, 50);
 	insert(head, 60);
 	insert(head, 70);
@@ -50,7 +89,9 @@ int main() {
 
 	display(head);
 	cout << endl;
+	cout << endl;
 
+	ListNode *sortedList = mergeSort(head);
 
-	display(head);
+	display(sortedList);
 }
