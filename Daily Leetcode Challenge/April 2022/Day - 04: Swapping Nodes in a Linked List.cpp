e7f99c9aff634d8a500c46bf7a2ddce36bcf2cd0
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-04-04 16:59:00 +0600

    Swapping Nodes in a Linked List

--- Daily Leetcode Challenge/April 2022/Day - 04: Swapping Nodes in a Linked List.cpp
+++ Daily Leetcode Challenge/April 2022/Day - 04: Swapping Nodes in a Linked List.cpp
@@ -11,6 +11,46 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
+        ListNode* left = head;
+        ListNode* right = head;
+        ListNode* cur = head;
+        
+        int count = 1;
+        while (cur != NULL) {
+            if (count < k) {
+                left = left->next;
+            } else if (count > k) {
+                right = right->next;
+            }
+            count++;
+            cur = cur->next;
+        }
+        
+        int temp = left->val;
+        left->val = right->val;
+        right->val = temp;
+        
+        return head;
+    }
+};
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
         ListNode* temp = head;
         ListNode* newHead = head;
         vector<int> v;
