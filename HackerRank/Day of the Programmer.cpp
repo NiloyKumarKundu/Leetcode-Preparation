c50fd5de0e4949f7235a0973c4962e6a8e07c922
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-04-13 21:07:03 +0600

    Day of the Programmer

---
+++ HackerRank/Day of the Programmer.cpp
@@ -0,0 +1,168 @@
+// Clean Code
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+string dayOfProgrammer(int year) {
+    string date = "";
+    string s_year = to_string(year);
+      
+    if (year == 1918) {
+        date = "26.09." + s_year;
+    } else if (year <= 1917) {
+        if (year % 4 == 0) {
+            date = "12.09." + s_year;
+        } else {
+            date = "13.09." + s_year;
+        }
+    } else {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            date = "12.09." + s_year;
+        } else {
+            date = "13.09." + s_year;
+        }
+    }
+    
+    return date;
+    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string year_temp;
+    getline(cin, year_temp);
+
+    int year = stoi(ltrim(rtrim(year_temp)));
+
+    string result = dayOfProgrammer(year);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+
+
+
+
+
+
+
+
+// 1st Approach
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+string dayOfProgrammer(int year) {
+    int normalDay = 28;
+    string date = "";
+    if (year % 4 == 0) {
+        if (year <= 1917 && year % 4 == 0) {
+            normalDay++;
+        }
+        else if ( year > 1918 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
+            normalDay++;
+        }
+    }
+    
+    
+    int sum = 31 + 31 + 30 + 31 + 30 + 31 + 31;
+    int findDate = 0;
+    if (year == 1918) {
+        date = "26.09." + to_string(year);
+    } else {
+        sum += normalDay;
+        findDate = 256 - sum;
+        date = to_string(findDate) + ".09." + to_string(year);
+    }
+    return date;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string year_temp;
+    getline(cin, year_temp);
+
+    int year = stoi(ltrim(rtrim(year_temp)));
+
+    string result = dayOfProgrammer(year);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
