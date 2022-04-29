60a57eabc1add99ef4548319b8407f28e586cfb6
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-04-29 22:24:16 +0600

    Shortest_Path_In_a_weighted_graph

---
+++ Advanced CP/Topics/Graph/Shortest Path Problem/Shortest_Path_In_a_weighted_graph.cpp
@@ -0,0 +1,167 @@
+// Time Complexity: O(N + E) * 2
+// Space Complexity: O(2 * N)
+// Axulary Space Complexity: O(N)           /// if use dfs, in bfs, no aux time com.
+
+
+#include <bits/stdc++.h>
+#define Niloy
+#define int int64_t
+#define mx (int) 1e5 + 123
+#define MOD (int) 1e9 + 7
+#define pb push_back
+#define pairs pair<int, int>
+#define vi vector<int>
+#define vb vector<bool>
+#define vii vector<pairs>
+#define lb lower_bound
+#define ub upper_bound
+#define endl '\n'
+#define llu unsigned long long
+using namespace std;
+/* ----------------------------------------------------------------------------------- */
+
+// Input/Output
+#define fastInput ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define square(a) (a * a)
+#define mem(a, b) memset(a, b, sizeof(a))
+
+// Fractional Number
+#define fraction()        cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed, ios::floatfield);
+
+// Direction Array
+int dx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+// File I/O
+#define read(x)	 freopen(x, "r", stdin);
+#define write(x) freopen(x, "w", stdout);
+ 
+// Loops
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define REP(i, a, n) for (int i = a; i <= n; i++)
+#define rev(i, n, a) for (int i = n - 1; i >= a; i--)
+#define REV(i, n, a) for (int i = n; i >= a; i--)
+ 
+/* ----------------------------------------------------------------------------------- */
+ 
+#define Cases  cout << "Case " << ++Case << ": ";
+#define __test int tt; int Case=0; cin >> tt; while(tt--)
+#define showTime cerr << "time = " << (clock() / CLOCKS_PER_SEC) << " sec" << '\n';
+ 
+#define dbgA2(A, n, m) {cout<<"--> "<<#A<<" = \n";rep(i, 0, n){rep(j, 0, m){cout<<A[i][j]<<"";}cout<<"\n";}cout<<"\n";}
+#define dbgA(A, n) {cout<<" --> "<<#A<<" = (";rep(i, 0, n)cout<<A[i]<<" ";cout<<")\n";}
+#define dbg(args...) {string sss(#args);sss+=',';cout<<" --> ";debugger::call(all(sss), args);cout<<"\n";}
+ 
+/* ----------------------------------------------------------------------------------- */
+ 
+int gcd(int n, int m) { return m ? gcd(m, n % m) : n; }
+int lcm(int n, int m) { return n / gcd(n, m) * m; }
+ 
+struct debugger {
+    typedef string::iterator si;
+    static void call(si it, si ed) {}
+    template<typename T, typename ... aT>
+    static void call(si it, si ed, T a, aT... rest) {
+        string b;
+        for(; *it!=','; ++it)
+            if(*it!=' ')
+                b+=*it;
+        cout << b << "=" << a << " ";
+        call(++it, ed, rest...);
+    }
+};
+
+/* ----------------------------------------------------------------------------------- */
+
+
+// Input Overloading
+
+template <typename T> 
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+
+/* ----------------------------------------------------------------------------------- */
+
+
+
+void input() {
+#ifdef Niloy
+    read("input.txt");  
+    write("output.txt");
+#endif
+}
+
+/* ----------------------------------------------------------------------------------- */
+
+vector<pairs> graph[mx];
+vi visited;
+vi dist;
+stack<int> st;
+int n, m;
+
+void topoSort(int node) {
+	visited[node] = 1;
+    for (auto i : graph[node]) {
+        if (!visited[i.first]) {
+			topoSort(i.first);
+		}
+	}
+	st.push(node);
+}
+
+void shortestPath(int src) {
+    visited.resize(n, 0);
+	dist.resize(n, INT_MAX);
+    
+    rep(i, 0, n) {
+        if (!visited[i]) {
+			topoSort(i);
+		}
+	}
+
+	dist[src] = 0;
+
+    while (!st.empty()) {
+		int node = st.top();
+		st.pop();
+
+        if (dist[node] != INT_MAX) {
+            for (auto i : graph[node]) {
+                if (dist[node] + i.second < dist[i.first]) {
+					dist[i.first] = dist[node] + i.second;
+				}
+			}
+        }
+	}
+}
+
+void solve() {
+	int src, dest;
+	cin >> n >> m >> src >> dest;
+
+    while (m--) {
+		int x, y, wt;
+		cin >> x >> y >> wt;
+		graph[x].push_back({y, wt}); 
+	}
+
+	shortestPath(src);
+	cout << dist[dest] << endl;
+}
+
+int32_t main() {
+    // input();
+    fastInput;
+    solve();
+
+    // __test {
+    // 	solve();
+    // }
+
+    // showTime;
+    return 0;
+}
