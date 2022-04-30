92c2f1b133fa5793f6f56949417c75741ec05047
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-04-30 12:18:13 +0600

    Dijkstra

---
+++ Advanced CP/Topics/Graph/Dijkstra/C. Dijkstra?.cpp
@@ -0,0 +1,180 @@
+// Time Complexity: O(N + E) * logN = NlogN
+// Space Complextiy: O(N) + O(N)
+
+
+#include <bits/stdc++.h>
+#define Niloy
+#define int int64_t
+#define mx (int) 1e5 + 123
+constexpr int64_t INF = std::numeric_limits<int64_t>::max();
+#define MOD (int) 1e9 + 7
+#define pb push_back
+#define pairs pair<int, int>
+#define vi vector<int>
+#define vb vector<bool>
+#define vii vector<pairs>
+#define lb lower_bound
+#define ub upper_bound
+#define endl '\n'
+#define llu unsigned long long
+using namespace std;
+/* ----------------------------------------------------------------------------------- */
+
+// Input/Output
+#define fastInput ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define square(a) (a * a)
+#define mem(a, b) memset(a, b, sizeof(a))
+
+// Fractional Number
+#define fraction()        cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed, ios::floatfield);
+
+// Direction Array
+int dx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+// File I/O
+#define read(x)	 freopen(x, "r", stdin);
+#define write(x) freopen(x, "w", stdout);
+ 
+// Loops
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define REP(i, a, n) for (int i = a; i <= n; i++)
+#define rev(i, n, a) for (int i = n - 1; i >= a; i--)
+#define REV(i, n, a) for (int i = n; i >= a; i--)
+ 
+/* ----------------------------------------------------------------------------------- */
+ 
+#define Cases  cout << "Case " << ++Case << ": ";
+#define __test int tt; int Case=0; cin >> tt; while(tt--)
+#define showTime cerr << "time = " << (clock() / CLOCKS_PER_SEC) << " sec" << '\n';
+ 
+#define dbgA2(A, n, m) {cout<<"--> "<<#A<<" = \n";rep(i, 0, n){rep(j, 0, m){cout<<A[i][j]<<"";}cout<<"\n";}cout<<"\n";}
+#define dbgA(A, n) {cout<<" --> "<<#A<<" = (";rep(i, 0, n)cout<<A[i]<<" ";cout<<")\n";}
+#define dbg(args...) {string sss(#args);sss+=',';cout<<" --> ";debugger::call(all(sss), args);cout<<"\n";}
+ 
+/* ----------------------------------------------------------------------------------- */
+ 
+int gcd(int n, int m) { return m ? gcd(m, n % m) : n; }
+int lcm(int n, int m) { return n / gcd(n, m) * m; }
+ 
+struct debugger {
+    typedef string::iterator si;
+    static void call(si it, si ed) {}
+    template<typename T, typename ... aT>
+    static void call(si it, si ed, T a, aT... rest) {
+        string b;
+        for(; *it!=','; ++it)
+            if(*it!=' ')
+                b+=*it;
+        cout << b << "=" << a << " ";
+        call(++it, ed, rest...);
+    }
+};
+
+/* ----------------------------------------------------------------------------------- */
+
+
+// Input Overloading
+
+template <typename T> 
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+
+/* ----------------------------------------------------------------------------------- */
+
+
+
+void input() {
+#ifdef Niloy
+    read("input.txt");  
+    write("output.txt");
+#endif
+}
+
+/* ----------------------------------------------------------------------------------- */
+
+vector<pairs> graph[mx];
+vi vis, par, dist;
+priority_queue<pairs, vector<pairs>, greater<pairs>> pq;
+int n, m;
+
+bool dijkstra(int src, int end) {
+	dist.resize(mx, INF);
+	par.resize(mx, 0);
+	vis.resize(mx, 0);
+	dist[src] = 0;
+	pq.push({0, src});
+	par[src] = -1;
+
+	while (!pq.empty()) {
+		int prevDist = pq.top().first;
+		int prev = pq.top().second;
+		pq.pop();
+
+        if (prev == end) {
+			return true;
+		}
+
+		vis[prev] = 1;
+		for (auto i : graph[prev]) {
+			int next = i.first;
+			int nextDist = i.second;
+
+            if (!vis[next] && dist[next] >= dist[prev] + nextDist) {
+				dist[next] = dist[prev] + nextDist;
+				pq.push({dist[next], next});
+				par[next] = prev;
+			}
+		}
+	}
+	return false;
+}
+
+
+
+
+void solve() {
+	cin >> n >> m;
+
+	while (m--) {
+		int x, y, wt;
+		cin >> x >> y >> wt;
+		graph[x].push_back({y, wt});
+		graph[y].push_back({x, wt});
+	}
+
+	if (dijkstra(1, n)) {
+		vi path;
+        int i = n;
+
+        while (i != -1) {
+            path.push_back(i);
+            i = par[i];
+        }
+
+        REV(i, path.size() - 1, 0) {
+            cout << path[i] << " ";
+        }
+        cout << endl;
+	} else {
+		cout << "-1\n";
+	}
+	
+}
+
+int32_t main() {
+    // input();
+    fastInput;
+    solve();
+
+    // __test {
+    // 	solve();
+    // }
+
+    // showTime;
+    return 0;
+}
