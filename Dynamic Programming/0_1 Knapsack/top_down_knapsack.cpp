c994e1a2216dee4e94dc6afb126237a13aa3524a
Niloy <nkundu191071@bscse.uiu.ac.bd>
2022-06-19 23:26:37 +0600

    0/1 Knapsack (Iterative)

---
+++ Dynamic Programming/0_1 Knapsack/top_down_knapsack.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+#define Niloy
+#define int int64_t
+#define mx (int) 1e5 + 123
+constexpr int64_t INF = std::numeric_limits<int64_t>::max();
+#define MOD (int) 1e9 + 7
+#define pb push_back
+#define pairs pair<int, int>
+#define vi vector<int>
+#define vb vector<bool>
+#define vii vector<pairs>
+#define lb lower_bound
+#define ub upper_bound
+#define endl '\n'
+#define llu unsigned long long
+using namespace std;
+/* ----------------------------------------------------------------------------------- */
+
+// Input/Output
+#define fastInput ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define square(a) (a * a)
+#define mem(a, b) memset(a, b, sizeof(a))
+
+// Fractional Number
+#define fraction()        cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed, ios::floatfield);
+
+// Direction Array
+int dx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+// File I/O
+#define read(x)	 freopen(x, "r", stdin);
+#define write(x) freopen(x, "w", stdout);
+ 
+// Loops
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define REP(i, a, n) for (int i = a; i <= n; i++)
+#define rev(i, n, a) for (int i = n - 1; i >= a; i--)
+#define REV(i, n, a) for (int i = n; i >= a; i--)
+ 
+/* ----------------------------------------------------------------------------------- */
+ 
+#define Cases  cout << "Case " << ++Case << ": ";
+#define __test int tt; int Case=0; cin >> tt; while(tt--)
+#define showTime cerr << "time = " << (clock() / CLOCKS_PER_SEC) << " sec" << '\n';
+ 
+#define dbgA2(A, n, m) {cout<<"--> "<<#A<<" = \n";rep(i, 0, n){rep(j, 0, m){cout<<A[i][j]<<"";}cout<<"\n";}cout<<"\n";}
+#define dbgA(A, n) {cout<<" --> "<<#A<<" = (";rep(i, 0, n)cout<<A[i]<<" ";cout<<")\n";}
+#define dbg(args...) {string sss(#args);sss+=',';cout<<" --> ";debugger::call(all(sss), args);cout<<"\n";}
+ 
+/* ----------------------------------------------------------------------------------- */
+ 
+int gcd(int n, int m) { return m ? gcd(m, n % m) : n; }
+int lcm(int n, int m) { return n / gcd(n, m) * m; }
+ 
+struct debugger {
+    typedef string::iterator si;
+    static void call(si it, si ed) {}
+    template<typename T, typename ... aT>
+    static void call(si it, si ed, T a, aT... rest) {
+        string b;
+        for(; *it!=','; ++it)
+            if(*it!=' ')
+                b+=*it;
+        cout << b << "=" << a << " ";
+        call(++it, ed, rest...);
+    }
+};
+
+/* ----------------------------------------------------------------------------------- */
+
+
+// Input Overloading
+
+template <typename T> 
+istream &operator>>(istream &istream, vector<T> &v) {
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+
+/* ----------------------------------------------------------------------------------- */
+
+
+
+void input() {
+#ifdef Niloy
+    read("input.txt");  
+    write("output.txt");
+#endif
+}
+
+/* ----------------------------------------------------------------------------------- */
+
+const int row = 1000, col = 1000;
+int t[row][col];
+
+void init() {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+            if (i == 0 || j == 0)
+				t[i][j] = 0;
+		}
+	}
+}
+
+void knapsack(vi wt, vi val, int W, int n) {
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < W + 1; j++) {
+            if (wt[i - 1] <= j) {
+				t[i][j] = max(val[i - 1] + t[i - 1][j - wt[i - 1]], t[i - 1][j]);
+			} else if (wt[i - 1] > j) {
+				t[i][j] = t[i - 1][j];
+			}
+		}
+	}
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	vi wt(n, 0), val(n, 0);
+	int W;
+	cin >> wt;
+	cin >> val;
+	cin >> W;
+
+	knapsack(wt, val, W, n);
+
+	cout << t[n][W] << endl;
+}
+
+int32_t main() {
+    // input();
+    fastInput;
+    solve();
+
+    // __test {
+    // 	solve();
+    // }
+
+    // showTime;
+    return 0;
+}
